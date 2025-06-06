796d8db6f9f111fc3bf05e421bfc2b464e8f56b4
root <root@ip-10-1-0-222.eu-west-2.compute.internal>
2025-06-06 08:31:45 +0000

    [Sync Iteration] javascript/scrabble-score/1

---
+++ solutions/javascript/scrabble-score/1/scrabble-score.js
@@ -0,0 +1,15 @@
+//
+// This is only a SKELETON file for the 'Scrabble Score' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const score = (word) => {
+  let oneP = (word.match(/[A|E|I|O|U|L|N|R|S|T]/gi) || []).length
+  let twoP = (word.match(/[D|G]/gi) || []).length * 2
+  let threeP = (word.match(/[B|C|M|P]/gi) || []).length * 3
+  let fourP = (word.match(/[F|H|V|W|Y]/gi) || []).length * 4
+  let fiveP = (word.match(/[K]/gi) || []).length * 5
+  let eightP = (word.match(/[J|X]/gi) || []).length * 8
+  let tenP = (word.match(/[Q|Z]/gi) || []).length * 10
+  return oneP + twoP + threeP + fourP + fiveP + eightP + tenP
+};
