8b283111d61f3e2f4d7d4b15d9f835eb24168cbe
root <root@ip-10-1-0-222.eu-west-2.compute.internal>
2025-06-06 08:31:53 +0000

    [Sync Iteration] javascript/resistor-color-duo/2

---
+++ solutions/javascript/resistor-color-duo/2/resistor-color-duo.js
@@ -0,0 +1,19 @@
+//
+// This is only a SKELETON file for the 'Resistor Color Duo' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const decodedValue = ([first, second]) => Number(COLORS.indexOf(first) * 10 + COLORS.indexOf(second))
+
+const COLORS = [
+      'black',
+      'brown',
+      'red',
+      'orange',
+      'yellow',
+      'green',
+      'blue',
+      'violet',
+      'grey',
+      'white',
+    ]
