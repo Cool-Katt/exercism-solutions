423e45c98942249dea7ffd019c38cf1a2d2e1f8a
root <root@ip-10-1-0-222.eu-west-2.compute.internal>
2025-06-06 08:32:24 +0000

    [Sync Iteration] fsharp/bob/1

---
+++ solutions/fsharp/bob/1/Bob.fs
@@ -0,0 +1,17 @@
+﻿module Bob
+
+open System
+
+let response (input: string): string =
+
+    let input = input.TrimEnd()
+
+    let isShout = String.exists Char.IsLetter input && input.ToUpper() = input
+
+    match (input = "", input.EndsWith("?"), isShout) with
+
+    | (_, true, true) -> "Calm down, I know what I'm doing!"
+    | (_, _, true) -> "Whoa, chill out!"
+    | (_, true, _) -> "Sure."
+    | (true, _, _) -> "Fine. Be that way!"
+    | _ -> "Whatever."
