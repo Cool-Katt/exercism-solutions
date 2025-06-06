5ec2474284c52860630f5d2e6789f6106b0748e5
root <root@ip-10-1-0-222.eu-west-2.compute.internal>
2025-06-06 08:32:10 +0000

    [Sync Iteration] clojure/difference-of-squares/1

---
+++ solutions/clojure/difference-of-squares/1/src/difference_of_squares.clj
@@ -0,0 +1,19 @@
+(ns difference-of-squares)
+
+(defn nums [n]
+  (range 1 (inc n))
+)
+
+(defn square [n]
+  (* n n)
+)
+
+(def sum (partial reduce +))
+
+(def square-of-sum (comp square sum nums))
+
+(def sum-of-squares (comp sum (partial map square) nums))
+
+(defn difference [n] 
+  (- (square-of-sum n) (sum-of-squares n))
+)
