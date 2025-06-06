0b199e03036dbf25f6d2fcb5826ce09a80e4b8a0
root <root@ip-10-1-0-222.eu-west-2.compute.internal>
2025-06-06 08:32:02 +0000

    [Sync Iteration] javascript/eliuds-eggs/3

---
+++ solutions/javascript/eliuds-eggs/3/eliuds-eggs.js
@@ -0,0 +1,7 @@
+//
+// This is only a SKELETON file for the 'Eliud's Eggs' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const eggCount = (displayValue) => 
+  [...displayValue.toString(2)].reduce((acc, curr) => curr === '1' ? acc += 1 : acc, 0)
