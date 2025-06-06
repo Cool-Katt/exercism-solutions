b264ebf5c0aa43fc85cce73c9ce4fa16c8b40a69
root <root@ip-10-1-0-222.eu-west-2.compute.internal>
2025-06-06 08:31:59 +0000

    [Sync Iteration] javascript/elyses-looping-enchantments/1

---
+++ solutions/javascript/elyses-looping-enchantments/1/enchantments.js
@@ -0,0 +1,33 @@
+// @ts-check
+
+/**
+ * Determine how many cards of a certain type there are in the deck
+ *
+ * @param {number[]} stack
+ * @param {number} card
+ *
+ * @returns {number} number of cards of a single type there are in the deck
+ */
+export function cardTypeCheck(stack, card) {
+  let num = 0
+  stack.forEach((el) => {
+    if (el === card) num++
+  })
+  return num
+}
+
+/**
+ * Determine how many cards are odd or even
+ *
+ * @param {number[]} stack
+ * @param {boolean} type the type of value to check for - odd or even
+ * @returns {number} number of cards that are either odd or even (depending on `type`)
+ */
+export function determineOddEvenCards(stack, type) {
+  let num = 0
+  for (let el of stack) {
+    let condition = type ? (el % 2 === 0) : (el % 2 !== 0)
+    condition && num++
+  }
+  return num
+}
