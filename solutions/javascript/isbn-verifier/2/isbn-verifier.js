996cd04c2df2d482d40a521039e4f06e1407474a
root <root@ip-10-1-0-222.eu-west-2.compute.internal>
2025-06-06 08:31:51 +0000

    [Sync Iteration] javascript/isbn-verifier/2

---
+++ solutions/javascript/isbn-verifier/2/isbn-verifier.js
@@ -0,0 +1,12 @@
+//
+// This is only a SKELETON file for the 'ISBN Verifier' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const isValid = (raw) => {
+  let formated = /^[\d|X|-]+$/.test(raw) ? raw.match(/\d|X$/g) : []
+  return [
+    digits => digits.length === 10,
+    digits => digits.reduce((sum, digit, i) => sum + (digit === 'X' ? 10 : digit) * (10 - i), 0) % 11 === 0
+  ].every(check => check(formated))
+};
