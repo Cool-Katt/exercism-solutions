9bc70b9aed6a132521e435ee084af17d2bc2119f
root <root@ip-10-1-0-222.eu-west-2.compute.internal>
2025-06-06 08:31:53 +0000

    [Sync Iteration] javascript/scale-generator/2

---
+++ solutions/javascript/scale-generator/2/scale-generator.js
@@ -0,0 +1,37 @@
+//
+// This is only a SKELETON file for the 'Scale Generator' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+const TONICS = ["F", "Bb", "Eb", "Ab", "Db", "Gb", "d", "g", "c", "f", "bb", "eb"]
+const STEPS = {
+    m: 1,
+    M: 2,
+    A: 3,
+}
+
+export class Scale {
+  static chromaticMaker(tonic) {
+    let pitches = (TONICS.includes(tonic)
+        ? ["A", "Bb", "B", "C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab"]
+        : ["A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"]);
+    let index = pitches.indexOf(tonic[0].toUpperCase() + tonic.slice(1));
+    return [...pitches.slice(index), ...pitches.slice(0, index)];
+  }
+  
+  constructor(tonic) {
+    this.tonic = tonic
+  }
+
+  chromatic() {
+    return Scale.chromaticMaker(this.tonic)
+  }
+
+  interval(intervals) {
+    let scale = this.chromatic()
+    return [scale[0], ...[...intervals]
+            .map((a) => (STEPS[a]))
+            .reduce((acc, cur) => acc.length === 0 ? [cur] : [...acc, acc[acc.length - 1] + cur], [])
+            .map((i) => scale[i % scale.length])];
+  }
+}
