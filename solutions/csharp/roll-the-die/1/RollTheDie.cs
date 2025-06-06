e5af7e8560e72cf2e9feee9c8184f46a42c4b22e
root <root@ip-10-1-0-222.eu-west-2.compute.internal>
2025-06-06 08:32:17 +0000

    [Sync Iteration] csharp/roll-the-die/1

---
+++ solutions/csharp/roll-the-die/1/RollTheDie.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class Player
+{
+    private Random rand = new Random();
+    
+    public int RollDie() => rand.Next(1, 19); 
+    
+    public double GenerateSpellStrength() => rand.NextDouble() * 100.0;
+}
