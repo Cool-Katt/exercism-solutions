7b3cb7065e7a2f5a195c1b3deb54342bcb2c3388
root <root@ip-10-1-0-222.eu-west-2.compute.internal>
2025-06-06 08:32:21 +0000

    [Sync Iteration] ruby/space-age/1

---
+++ solutions/ruby/space-age/1/space_age.rb
@@ -0,0 +1,7 @@
+class SpaceAge
+  Planet = {Earth: 1, Mercury: 0.2408467, Venus:  0.61519726, Mars: 1.8808158, Jupiter: 11.862615, Saturn:  29.447498, Uranus:  84.016846, Neptune: 164.79132}
+  Planet.each{|planet,year| define_method("on_" + planet.downcase.to_s){ @earth_year / Planet[planet]}}
+  def initialize(second)
+    @earth_year = second.to_f / 60 / 60 / 24 / 365.25
+  end
+end
