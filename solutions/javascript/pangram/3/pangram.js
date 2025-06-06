a116adffd39ec1e2af477766cb445e6337e4cf1d
root <root@ip-10-1-0-222.eu-west-2.compute.internal>
2025-06-06 08:31:41 +0000

    [Sync Iteration] javascript/pangram/3

---
+++ solutions/javascript/pangram/3/pangram.js
@@ -0,0 +1,6 @@
+//
+// This is only a SKELETON file for the 'Pangram' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const isPangram = (str) => new Set(str.toLowerCase().match(/[a-z]/g)).size === 26
