f9ed4a04f95ec10ce7523cae6148389bff1e37b4
root <root@ip-10-1-0-222.eu-west-2.compute.internal>
2025-06-06 08:32:05 +0000

    [Sync Iteration] python/largest-series-product/4

---
+++ solutions/python/largest-series-product/4/largest_series_product.py
@@ -0,0 +1,14 @@
+from math import prod
+
+def largest_product(series, size):
+    if size == 0:
+        return 1
+    if len(series) < size:
+        raise ValueError("span must be smaller than string length")
+    if not series.isdigit():
+        raise ValueError("digits input must only contain digits")
+    if  size < 0:
+        raise ValueError("span must not be negative")
+    
+    toInt = [int(item) for item in series]
+    return max((prod(toInt[i : i + size]) for i in range(len(series) - size + 1)))
