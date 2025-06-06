cf4bb178bc4dabbd849814b36d8b775a8c9de61b
root <root@ip-10-1-0-222.eu-west-2.compute.internal>
2025-06-06 08:31:59 +0000

    [Sync Iteration] javascript/elyses-transformative-enchantments/2

---
+++ solutions/javascript/elyses-transformative-enchantments/2/enchantments.js
@@ -0,0 +1,73 @@
+// @ts-check
+
+/**
+ * Double every card in the deck.
+ *
+ * @param {number[]} deck
+ *
+ * @returns {number[]} deck with every card doubled
+ */
+export const seeingDouble = (deck) => deck.map(v => v * 2)
+
+/**
+ *  Creates triplicates of every 3 found in the deck.
+ *
+ * @param {number[]} deck
+ *
+ * @returns {number[]} deck with triplicate 3s
+ */
+export const threeOfEachThree = (deck) => deck.flatMap(v => v === 3 ? [v, v, v] : v)
+
+/**
+ * Extracts the middle two cards from a deck.
+ * Assumes a deck is always 10 cards.
+ *
+ * @param {number[]} deck of 10 cards
+ *
+ * @returns {number[]} deck with only two middle cards
+ */
+export function middleTwo(deck) {
+  let middle = Math.floor(deck.length / 2)
+  return deck.slice(middle - 1, middle + 1)
+}
+
+/**
+ * Moves the outside two cards to the middle.
+ *
+ * @param {number[]} deck with even number of cards
+ *
+ * @returns {number[]} transformed deck
+ */
+
+export function sandwichTrick(deck) {
+  let [top, bottom] = [deck.shift(), deck.pop()]
+  deck.splice(Math.floor(deck.length / 2), 0, bottom, top)
+  return deck
+}
+
+/**
+ * Removes every card from the deck except 2s.
+ *
+ * @param {number[]} deck
+ *
+ * @returns {number[]} deck with only 2s
+ */
+export const twoIsSpecial = (deck) => deck.filter(v => v === 2)
+
+/**
+ * Returns a perfectly order deck from lowest to highest.
+ *
+ * @param {number[]} deck shuffled deck
+ *
+ * @returns {number[]} ordered deck
+ */
+export const perfectlyOrdered = (deck) => deck.sort((a,b) => a - b)
+
+/**
+ * Reorders the deck so that the top card ends up at the bottom.
+ *
+ * @param {number[]} deck
+ *
+ * @returns {number[]} reordered deck
+ */
+export const reorder = (deck) => deck.reverse()  
