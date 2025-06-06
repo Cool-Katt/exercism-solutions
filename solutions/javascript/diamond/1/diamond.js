38d70437a04895c2394b48d7f64953a893dc1c38
root <root@ip-10-1-0-222.eu-west-2.compute.internal>
2025-06-06 08:31:49 +0000

    [Sync Iteration] javascript/diamond/1

---
+++ solutions/javascript/diamond/1/diamond.js
@@ -0,0 +1,16 @@
+//
+// This is only a SKELETON file for the 'Diamond' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const rows = (text) => {
+  const limit = text.charCodeAt(0) - 65
+  let spaces = ' '.repeat(limit)
+  const result = [spaces + 'A' + spaces]
+  for (let i = 1; i <= limit; i++) {
+    spaces = ' '.repeat(limit - i)
+    const char = String.fromCharCode(i + 65)
+    result.push(spaces + char + ' '.repeat(2 * i - 1) + char + spaces)
+  }
+  return result.concat(result.slice(0, -1).reverse())
+};
